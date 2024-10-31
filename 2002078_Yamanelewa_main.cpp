aaf40ab4e0d5b214106a2e9ea1c4b28b6451d369
yaman <yamanelewa@gmail.com>
2024-10-31 07:01:13 -0700

    version 1

---
+++ 2002078_Yamanelewa_main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+int summation(int a ,int b)
+{
+   int z;
+
+   z=a+b;
+
+   return z;
+}
+
+
+
+
+int main()
+{
+
+
+  int x=10;
+
+  int y=20;
+
+
+  int result;
+
+  result = summation(x,y);
+
+
+  cout<< "The summation ="<<result<<endl;
+
+
+
+}
